38736ab9e67532fd6ad52fb6dcd170a2d529a297
CAL <chrisal20120@gmail.com>
2025-09-08 19:44:24 +0200

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /run/media/chrisal20120/WD_BLACK/Christopher_LUCAS/Fireflies/Fireflies
-# Build directory: /run/media/chrisal20120/WD_BLACK/Christopher_LUCAS/Fireflies/Fireflies/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
