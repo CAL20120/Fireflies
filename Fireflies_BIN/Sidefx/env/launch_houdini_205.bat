6bbfb593a554e556ffcdc4ef71c418a4888b8ee0
CAL <chrisal20120@gmail.com>
2025-10-12 20:18:34 +0200

    changed houdini splashscreen

--- Fireflies_BIN/Sidefx/env/launch_houdini_205.bat
+++ Fireflies_BIN/Sidefx/env/launch_houdini_205.bat
@@ -2,4 +2,6 @@
 for /f "tokens=*" %%a in (C:\Fireflies\Fireflies_BIN\Sidefx\env\houdini.env) do set %%a
 @REM set HOUDINI_PATH=C:\Fireflies\Fireflies_BIN\Sidefx
 @REM set HOUDINI_TOOLBAR_PATH=C:\Fireflies\Fireflies_BIN\Sidefx\toolbar;&
+set HOUDINI_SPLASH_FILE=C:\Fireflies\Fireflies\softs_logo\fireflies_splash_macro.png
+set HOUDINI_SPLASH_MESSAFE="COUCOU"
 start "" "C:\\Fireflies\\Common\\Houdini_vars\\houdini_205445\\Houdini20.5.445\\bin\\houdini.exe"
