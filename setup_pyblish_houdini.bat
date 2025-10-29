d7ab71cc4b74f126f41caa2fdf3c4ae3c8d6b512
CAL <chrisal20120@gmail.com>
2025-10-29 15:38:53 +0100

    added the synology api support for the lib importer

--- setup_pyblish_houdini.bat
+++ setup_pyblish_houdini.bat
@@ -1,1 +1,3 @@
 "C:\Fireflies\Common\Houdini_vars\houdini_205445\Houdini20.5.445\bin\hython.exe" -m pip install pyblish-base pyblish-houdini pyblish-lite
+
+"C:\Fireflies\Common\Houdini_vars\houdini_205445\Houdini20.5.445\bin\hython.exe" -m pip install synology-api
