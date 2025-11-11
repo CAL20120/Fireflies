3a3fc69ead40c990a53be9ec521e2d33b235346a
CAL <chrisal20120@gmail.com>
2025-11-11 17:11:21 +0100

    added udim detection and renderman tx generation

--- Fireflies_BIN/fireflies/houdini/texture_linker.py
+++ Fireflies_BIN/fireflies/houdini/texture_linker.py
@@ -2,9 +2,12 @@ from PySide2 import QtCore
 from PySide2 import QtWidgets
 from PySide2 import QtGui
 
-
 import sys 
 import os
+
+import subprocess
+import glob
+import re
 
 import random
 
@@ -34,27 +37,30 @@ class texture_linker_window(QtWidgets.QDialog):
         self.rman_opt.setChecked(True)
     
         self.explorer_button = QtWidgets.QPushButton()
-        self.explorer_button.setIcon(QtGui.QIcon("hicon:/SVGIcons.index?BUTTONS_chooser_folder.svg"))
+        self.explorer_button.setIcon(hou.ui.createQtIcon("BUTTONS_chooser_folder"))
 
         self.import_btn = QtWidgets.QPushButton("Import textures")
         self.close_button = QtWidgets.QPushButton("Close")
-        self.debug = QtWidgets.QPushButton("debug")
+        # self.debug = QtWidgets.QPushButton("debug")
 
     
     def create_layout(self):
         self.explorer_layout = QtWidgets.QHBoxLayout()
         # self.explorer_layout.addWidget(self.file_path_edit)
         self.explorer_layout.addWidget(self.files_sel)
+
         self.explorer_layout.addWidget(self.explorer_button)
+        self.explorer_button.setToolTip("Explore files")
 
         self.radio_layout = QtWidgets.QVBoxLayout()
         self.radio_layout.addWidget(self.rman_opt)
+        # self.radio_layout.addWidget(udim_check)
 
         self.button_layout = QtWidgets.QHBoxLayout()
         self.button_layout.addStretch()
         self.button_layout.addWidget(self.import_btn)
         self.button_layout.addWidget(self.close_button)
-        self.button_layout.addWidget(self.debug)
+        # self.button_layout.addWidget(self.debug)
 
         self.main_layout = QtWidgets.QVBoxLayout(self)
         self.main_layout.addLayout(self.explorer_layout)
@@ -116,18 +122,74 @@ class create_hou_matlib():
         self.layer_inputs = self.pxr_layer.inputNames()
         self.surface_inputs = self.pxr_surface.inputNames()
 
-    def link_textures(self, tex_name:str, tex_path:str, input_value:str):
+
+    def check_udim_presence(self, texture_path:str, tex_name:str):
+        udim_check = False
+
+        dir_path = os.path.dirname(texture_path)        
+        target_pattern = f"{tex_name}.[1-9][0-9][0-9][0-9].exr"
+        udim_file_pattern = os.path.join(dir_path, target_pattern)
+
+        tex_udim_files = []
+        udim_index = []
+        pattern_search = glob.glob(udim_file_pattern, recursive=True)
+
+        for file in pattern_search:
+            udim_num = file.split('.')[-2]
+            print(udim_num)
+            udim_index.append(udim_num)
+
+            tex_udim_files.append(file)
+
+            if len(tex_udim_files) >= 1:
+                udim_check = True
+
+        return udim_check, udim_index
+
+
+
+    def link_textures(self, tex_name:str, texture_path:str, input_value:str):
+
+        udim_check, udim_index = self.check_udim_presence(texture_path=texture_path, tex_name=tex_name)
+
+        if not os.path.exists(texture_path):
+            print("Cannot find file")
+            return
+
+
+
+        if udim_check == False:
+            print('using single file')
+            texture_path = rman_generate_tx().make_tx(input_file=texture_path)
+        
+
+        else:
+            print('using udim files')
+
+            base_path = rman_generate_tx().make_udim_tx(input_file=texture_path)
+
+            print(type(base_path))
+
+            if udim_index:
+                texture_path = re.sub(udim_index[0], "<UDIM>", base_path)
+
+                    
+
         output_collect = hou.node(f"{self.pxr_node.path()}/output_collect")
         print(self.pxr_surface.path())
         output_collect.setInput(0, self.pxr_surface)
 
         tex_node = self.pxr_node.createNode('pxrtexture', tex_name)
-        tex_node.parm('filename').set(tex_path)
+
+
+        tex_node.parm('filename').set(texture_path)
+
 
         tex_outputs = tex_node.outputNames()
         tex_target_output = tex_outputs.index('resultRGB')
 
-        if not "OPACITY" in tex_path:
+
+        if not "OPACITY" in texture_path:
             target_input = self.layer_inputs.index(input_value)
             self.pxr_layer.setInput(target_input, tex_node, tex_target_output)
 
@@ -136,67 +198,185 @@ class create_hou_matlib():
             self.pxr_surface.setInput(target_input, tex_node, tex_target_output)
 
 
-        if any(name in tex_path for name in ['ROUGHNESS', 'NORMAL', 'Metalness']):
+        if any(name in texture_path for name in ['ROUGHNESS', 'NORMAL', 'Metalness']):
             if tex_node:
                 tex_node.parm("filename_colorspace").set('data')
 
-        if any(name in tex_path for name in ['COLOR', 'OPACITY', 'SSS']):
+        if any(name in texture_path for name in ['COLOR', 'OPACITY', 'SSS']):
             if tex_node:
                 tex_node.parm("filename_colorspace").set('srgb_texture')
 
 
 
-    def create_hou_tree(self, current_items:list):
+    def create_hou_tree(self, current_items:list) -> hou:
         
         # print(current_items)
         for tex in current_items:
 
             if "COLOR" in tex:
-                self.link_textures(tex_name="color", tex_path=tex, input_value="diffuseColor")
+                if not hasattr(self, 'color_node'):
+                    self.link_textures(tex_name="color", texture_path=tex, input_value="diffuseColor")
+                    setattr(self, 'color_node', tex)            
 
-            
             if "Metalness" in tex:
-                self.link_textures(tex_name='metalness', tex_path=tex, input_value="specularFaceColor")
+                if not hasattr(self, 'metal_node'):
+                    self.link_textures(tex_name='metalness', texture_path=tex, input_value="specularFaceColor")
+                    setattr(self, 'metal_node', tex)            
 
 
             if "ROUGHNESS" in tex:
-                self.link_textures(tex_name="roughness", tex_path=tex, input_value="diffuseRoughness")
+                if not hasattr(self, 'rough_node'):
+                    self.link_textures(tex_name="Roughness", texture_path=tex, input_value="diffuseRoughness")
+                    setattr(self, 'rough_node', tex)            
+
 
             if "SSS" in tex:
-                self.link_textures(tex_name="sss", tex_path=tex, input_value="subsurfaceColor")
-                self.pxr_layer.parm('enableSubsurface').set(1)
+                if not hasattr(self, 'sss_node'):
+                    self.link_textures(tex_name="sss", texture_path=tex, input_value="subsurfaceColor")
+                    self.pxr_layer.parm('enableSubsurface').set(1)
+                    setattr(self, 'sss_node', tex)            
+
 
             if "OPACITY" in tex:
-                self.link_textures(tex_name="opacity", tex_path=tex, input_value="presence")
+                if not hasattr(self, 'presence_node'):
+                    self.link_textures(tex_name="opacity", texture_path=tex, input_value="presence")
+                    setattr(self, 'presence_node', tex)            
 
 
             #the normal map requires extra nodes to work, so we create the ndoes manually
             if "NORMAL" in tex:
-                bump_layer = self.pxr_node.createNode('pxrbumpmixer', 'bump_mixer')
-                
-                bump_layer_outputs = bump_layer.outputNames()
-                target_output = bump_layer_outputs.index('resultN')
-                target_input = self.layer_inputs.index('bumpNormal')
+                if not os.path.exists(tex):
+                    return
 
-                self.pxr_layer.setInput(target_input, bump_layer, target_output)
-                
-                tex_node = self.pxr_node.createNode('pxrtexture', 'normal')
-                tex_node.parm('filename').set(tex)
-                tex_node.parm("filename_colorspace").set('data')
+                if not hasattr(self, 'normal_node'):
+                    udim_check, udim_index = self.check_udim_presence(texture_path=tex, tex_name="normal")
 
 
-                tex_outputs = tex_node.outputNames()
-                tex_target_output = tex_outputs.index('resultRGB')
+                    bump_layer = self.pxr_node.createNode('pxrbumpmixer', 'bump_mixer')
+                    
+                    bump_layer_outputs = bump_layer.outputNames()
+                    target_output = bump_layer_outputs.index('resultN')
+                    target_input = self.layer_inputs.index('bumpNormal')
 
-                bump_layer_inputs = bump_layer.inputNames()
-                target_input = bump_layer_inputs.index('surfaceGradient1')
+                    self.pxr_layer.setInput(target_input, bump_layer, target_output)
+                    
+                    tex_node = self.pxr_node.createNode('pxrtexture', 'normal')
 
-                bump_layer.setInput(target_input, tex_node, tex_target_output)
-                
+                    if not udim_check:    
+                        tex_node.parm('filename').set(tex)
+                    else:
+                        texture_path = tex
+                        base_path = rman_generate_tx().make_udim_tx(input_file=texture_path)
+
+                        texture_path = re.sub(udim_index[0], "<UDIM>", base_path)
+
+                        tex_val_input = texture_path
+
+                        tex_node.parm('filename').set(tex_val_input)
+                    
+                    
+                    tex_node.parm("filename_colorspace").set('data')
 
 
+                    tex_outputs = tex_node.outputNames()
+                    tex_target_output = tex_outputs.index('resultRGB')
+
+                    bump_layer_inputs = bump_layer.inputNames()
+                    target_input = bump_layer_inputs.index('surfaceGradient1')
+
+                    bump_layer.setInput(target_input, tex_node, tex_target_output)
+
+                    setattr(self, 'normal_node', tex)            
+                    
+
+
+class rman_generate_tx():
+    def __init__(self):
+        self.tx_make = "C:\\Program Files\\Pixar\\RenderManProServer-26.3\\bin\\txmake.exe"
+
+
+    def make_tx(self, input_file:str):
+
+        file_dir = os.path.dirname(input_file.replace("\\", "\\"))
+        path_tex = "{}.tex".format(file_dir)
+        
+        # if os.path.exists(path_tex):
+        #     return
+
+        subprocess.Popen(
+            [
+                self.tx_make, 
+                input_file,
+                path_tex
+            ],
+            shell=True, stdout=subprocess.PIPE
+        )
+
+        print("tx file generated at {}".format(path_tex))
+
+        return path_tex
+    
+    def make_udim_tx(self, input_file:str):
+
+        print("### CREATING UDIM TEX ###")
+        # file_dir = os.path.dirname(input_file.replace("\\", "/"))
+
+        # file_dir = os.path.dirname()
+
+        dir_path = os.path.dirname(input_file)
+
+        tex_name = input_file.rsplit('.', 2)[0]
+
+        target_pattern = f"{tex_name}.[1-9][0-9][0-9][0-9].exr"
+
+        udim_file_pattern = os.path.join(dir_path, target_pattern)
+
+
+        tex_udim_files = []
+        pattern_search = glob.glob(udim_file_pattern, recursive=True)
+
+        print("### {}".format(input_file))
+
+        for file in pattern_search:
+            print("### {}".format(file.replace('\\', '/')))
+
+            if not file:
+                continue
+
+
+            tex_udim_files.append(file.replace('\\', '/'))
+        
+
+        if not tex_udim_files:
+            raise RuntimeError("No udim files detected")
+            return
+        
+        sorted(tex_udim_files)
+        
+        tex_udim_output = []
+        for index, tex in enumerate(tex_udim_files):
+            tex_name = tex.rsplit(".", 1)[0]
+
+            tex_udim_output.append(f"{tex_name}.tex")
+
+            subprocess.Popen(
+                [
+                    self.tx_make,
+                    tex,
+                    tex_udim_output[index]
+                ],
+                shell=True, stdout=subprocess.PIPE
+            )
+
+            print(tex_udim_output[index])
+
+
+        print(len(tex_udim_files))
+
+        return tex_udim_output[0]
 
 
 if __name__ == "__main__":
     x = texture_linker_window()
     x.show()
+
