9284310cf88cf9acb4c61f9bac7e78b29d1c0398
CAL <chrisal20120@gmail.com>
2025-12-16 11:22:09 +0100

    minor change

--- Fireflies_BIN/fireflies/fireflies_utils/usd/view_usd_file.bat
+++ Fireflies_BIN/fireflies/fireflies_utils/usd/view_usd_file.bat
@@ -2,4 +2,8 @@
 
 set PY="%LOCALAPPDATA%\Programs\Python\Python310\python.exe"
 
+set PYTHONPATH=C:\Fireflies\Fireflies_BIN;C:\Fireflies\Fireflies_BIN\usd_build_local\lib\python
+set Path=C:\Users\VFX\AppData\Local\Programs\Python\Python310\Scripts\;C:\Users\VFX\AppData\Local\Programs\Python\Python310\;C:\Fireflies\Fireflies_BIN\usd_build_local\bin;C:\Fireflies\Fireflies_BIN\usd_build_local\lib
+
 %PY% "C:\Fireflies\Fireflies_BIN\fireflies\fireflies_utils\usd\fireflies_usd_viewer.py" -asset_path "%~1"
+
