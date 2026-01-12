43d3d164de770cc7aba9ee83e80cac2eb2a2cc8a
CAL <chrisal20120@gmail.com>
2026-01-12 05:54:01 +0100

    fixed issues with the local usd viewer -- Some models wouldn't appear (due to stage loading or time code issues), changed the renderer plugin to Storm and adding visual improvements (hdri, colorspace, ambient occlusion...)

--- Fireflies_BIN/fireflies/fireflies_utils/usd/fireflies_usd_viewer.py
+++ Fireflies_BIN/fireflies/fireflies_utils/usd/fireflies_usd_viewer.py
@@ -16,9 +16,9 @@ from PySide2 import QtCore
 from PySide2 import QtWidgets
 from PySide2 import QtGui
 
-from pxr import Usd, UsdGeom, Sdf, UsdUtils
+from pxr import Usd, UsdGeom, Sdf, UsdUtils, UsdLux
 from pxr.Usdviewq.stageView import StageView
-
+from pxr.UsdAppUtils.complexityArgs import RefinementComplexities
 
 app = QtWidgets.QApplication.instance()
 
@@ -43,9 +43,20 @@ class Usd_Viewer_hou(QtWidgets.QWidget):
 
     def set_stage(self, stage):
         self.model.stage = stage
-        self.view.updateView(resetCam=True)
+        self.view.updateView(resetCam=True, forceComputeBBox=True)
+
+        f_start = stage.GetStartTimeCode()
+        self.model.currentFrame = Usd.TimeCode(f_start)
 
         data_model = self.view._dataModel.viewSettings
+        data_model.rendererPlugin = "HdStormRendererPlugin"
+
+        data_model.ambientOcclusion = True
+        data_model.enableShadows = True
+
+        data_model.colorCorrectionMode = 'sRGB'
+
+        data_model.complexity = RefinementComplexities.VERY_HIGH
 
         data_model.renderPurpose = True
         data_model.proxyPurpose = False
@@ -53,8 +64,25 @@ class Usd_Viewer_hou(QtWidgets.QWidget):
 
         view_settings = self.view._dataModel.viewSettings.freeCamera
 
-        view_settings.overrideNear = 0.001
-        view_settings.overrideFar = 10000.0
+        view_settings.overrideNear = 0.1
+        view_settings.overrideFar = 1000.0
+
+
+        #adding a simple hdri from nas lib
+        hdri_path = r"Z:\\VFX_LIB\\04_LIGHTING\\HDRI\\brown_photostudio_06_4k_LIGHT.exr"
+        try:
+            target_layer = stage.GetSessionLayer()
+            with Usd.EditContext(stage, target_layer):
+                hdri_prim = UsdLux.DomeLight.Define(stage, '/hdri')
+
+                if os.path.exists(hdri_path):
+                    hdri_prim.CreateTextureFileAttr(hdri_path)
+                    hdri_prim.CreateTextureFormatAttr(UsdLux.Tokens.latlong)
+
+        except:
+            print("Couldn't reach NAS")
+            pass
+
 
 
     def create_layout(self):
@@ -78,12 +106,14 @@ def launch_app(asset_path):
     if os.path.exists(asset_path):
         with Usd.StageCacheContext(UsdUtils.StageCache.Get()):
             stage = Usd.Stage.Open(asset_path)
-    
+
     else:
         print("path does not exist")
-        return
-    
     x = Usd_Viewer_hou(stage)
+
+    if not os.path.exists(asset_path):
+        print("Invalid path")
+        return
 
 
     window = Usd_Viewer_hou(stage=stage, asset_path=asset_path)
@@ -106,5 +136,5 @@ def main():
     launch_app(args.asset_path)
 
 
-if __name__ == "__main__":        
+if __name__ == "__main__": 
     main()
