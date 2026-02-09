d5bae5f7ce4abb3fe2d7ff6cb451a045db5ac20a
CAL <chrisal20120@gmail.com>
2026-02-09 06:19:15 +0100

    changed the houdini splashscreen

--- Fireflies_BIN/Sidefx/env/launch_houdini_205_rm27.bat
+++ Fireflies_BIN/Sidefx/env/launch_houdini_205_rm27.bat
@@ -5,6 +5,6 @@ set "DEADLINE_HOU=Z:\Deadline_DB\db_main\submission\Houdini\Client"
 set PYTHONPATH=%DEADLINE_HOU%;%PYTHONPATH%
 
 set "HOUDINI_PATH=%DEADLINE_HOU%;%HOUDINI_PATH%;&"
-set HOUDINI_SPLASH_FILE=C:\Fireflies\Fireflies\softs_logo\fireflies_splash_rm27.png
+set HOUDINI_SPLASH_FILE=C:\Fireflies\Fireflies\softs_logo\fireflies_splash_macro.png
 
 start "" "C:\\Fireflies\\Common\\Houdini_vars\\houdini_205445\\Houdini20.5.445\\bin\\houdini.exe"
